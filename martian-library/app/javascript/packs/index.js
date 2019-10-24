a456c3d7a6dde6116feeb5e6ba796992cd99d807
Dave Schlauderaff <dschlaud@gmail.com>
2019-10-23 20:01:31 -0500

    add Library query component

--- martian-library/app/javascript/packs/index.js
+++ martian-library/app/javascript/packs/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { render } from "react-dom";
 import Provider from '../components/Provider'
+import Library from '../components/Library'
 
 render(<Provider>
-'👻'
+  <Library />
 </Provider>, document.querySelector('#root'))
