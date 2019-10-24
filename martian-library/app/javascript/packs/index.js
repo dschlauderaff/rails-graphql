9fb014636f63ac5bbac0ab233732b60f3cb66b9c
Dave Schlauderaff <dschlaud@gmail.com>
2019-10-23 19:51:16 -0500

    create Provider component

--- martian-library/app/javascript/packs/index.js
+++ martian-library/app/javascript/packs/index.js
@@ -1,1 +1,7 @@
-console.log('👻');
+import React from 'react'
+import { render } from "react-dom";
+import Provider from '../components/Provider'
+
+render(<Provider>
+'👻'
+</Provider>, document.querySelector('#root'))
